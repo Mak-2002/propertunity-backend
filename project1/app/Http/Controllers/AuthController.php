5335ebea4c5638620c447bba1ad33b0f3021661d
Qusai Nasr <kossay.ar.en@gmail.com>
2023-07-28 16:14:07 +0300

    modified login and verification methods
    Added login after registeration
    and added validator feature

--- project1/app/Http/Controllers/AuthController.php
+++ project1/app/Http/Controllers/AuthController.php
@@ -7,28 +7,31 @@ use App\Models\VerificationCode;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+use function GuzzleHttp\is_host_in_noproxy;
+use function PHPUnit\Framework\returnSelf;
+
 // use Twilio\Rest\Client;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
-     {  
-       
-        $var = $request->validate([
+    {
+
+        $validator = validator($request->only('name', 'phone', 'password'), [
             'name' => 'required|string',
-            'phone' => ['required','unique:users', 'regex:/^[0-9+]+$/'],
+            'phone' => ['required', 'unique:users', 'regex:/^[0-9+]+$/'],
             'password' => 'min:6',
         ]);
 
         // we will assume that frontend checked for all rules except for uniqueness
-        if(!$var)
+        if ($validator->fails())
             return response()->json([
                 'status' => false,
                 'message' => 'A user with the entered phone number already exists',
             ], 401);
-       
-       // $authToken = $user->createToken('auth-token')->plainTextToken;
-        $sms=$this->sendSMS($request->phone);
+
+        // $authToken = $user->createToken('auth-token')->plainTextToken;
+        $sms = $this->sendSMS($request->phone);
         return response([
             'status' => true,
             'message' => 'Waiting for OTP verification',
@@ -46,42 +49,43 @@ class AuthController extends Controller
             'status' => true,
             'message' => 'Logged out successfully',
         ]);
-
     }
 
     ///login screen 1111111
     public function login(Request $request)
     {
-
-        request()->validate([
+        // Qusai: used Laravel's validator to access Validator::fails() method
+        $validator = validator($request->only('phone', 'password'), [
             'phone' => ['required', 'regex:/^[0-9+]+$/'],
             'password' => 'min:6',
         ]);
 
-        $fields = request(['phone', 'password']);
-        if (!auth()->attempt($fields)) {
-            return response()->json(['status' => false,'message' => 'Invalid credentials'], 401);
-            // Edited by qusai to return response code 401 (unauthorized) when entering wrong credentials
-        }
+        if ($validator->fails())
+            return response()->json([
+                'status' => false,
+                'message' => 'Invalid credentials'
+            ], 401);
 
-        $user = Auth::user();
-        $authToken = $user->createToken('auth-token')->plainTextToken;
+        if (!auth()->attempt(request()->only('phone', 'password')))
+            return response()->json([
+                'status' => false,
+                'message' => 'User with phone not found Or Wrong password'
+            ], 401);
 
         return response([
             'status' => true,
             'message' => 'Logged in successfully',
-            'access_token' => $authToken
+            'access_token' => Auth::user()->createToken('auth-token')->plainTextToken,
         ]);
-        
     }
 
     //send the otp 2222222
     public function sendSMS(string $phone)
-    {  
+    {
         $basic = new \Vonage\Client\Credentials\Basic("9d3f82fc", "D6EPxByXtmjr0bUU");
         $client = new \Vonage\Client($basic);
-    // $user = User::where('id', $id)->first();
-    //    $phone=$user->phone;
+        // $user = User::where('id', $id)->first();
+        //    $phone=$user->phone;
         # User Does not Have Any Existing OTP
         $verificationCode = VerificationCode::where('phone', $phone)->latest()->first();
 
@@ -92,21 +96,21 @@ class AuthController extends Controller
         }
 
         // Create a New OTP
-        else{
-        $verificationCode = VerificationCode::create([
-            'phone' => $phone,
-            'otp' => 111111, // DEBUG: Set by qusai for testing purpose
-            // 'otp' => rand(111111, 999999), //PRODUCTION
-            'expire_at' => now()->addMinutes(10),
-        ]);
-        $otp= $verificationCode->otp;
+        else {
+            $verificationCode = VerificationCode::create([
+                'phone' => $phone,
+                'otp' => 111111, // DEBUG: Set by qusai for testing purpose
+                // 'otp' => rand(111111, 999999), //PRODUCTION
+                'expire_at' => now()->addMinutes(10),
+            ]);
+            $otp = $verificationCode->otp;
         }
-        
-        
+
+
         $response = $client->sms()->send(
             new \Vonage\SMS\Message\SMS($phone, 'hi', $otp)
         );
-        
+
         $message = $response->current();
 
         if ($message->getStatus() == 0) {
@@ -123,26 +127,31 @@ class AuthController extends Controller
 
     //////verification of the otp 44444444
     public function verification(Request $request)
-    { 
+    {
         #Validation
-        $v=$request->validate([
+        $validator = validator($request->only('name', 'phone', 'otp', 'password'), [
             'name' => 'required',
             'phone' => ['required', 'regex:/^[0-9+]+$/'],
-            'otp' => ['required','regex:/^[0-9]+$/','digits:6'],
+            'otp' => ['required', 'regex:/^[0-9]+$/', 'digits:6'],
             'password' => 'min:6',
         ]);
-        
+
+        if ($validator->fails())
+            return response([
+                'success' => false,
+                'message' => 'Invalid credentials',
+            ], 401);
 
         #Validation Logic
         $verificationCode = VerificationCode::where('phone', $request->phone)->where('otp',  $request->otp)->first();
-          
+
         $now = now();
         $data = [
             'status' => false
         ];
         if (!$verificationCode) {
             // return redirect()->back()->with('error', 'Your OTP is not correct');
-            $data['message'] = 'Your OTP is not correct';
+            $data['message'] = 'OTP not creaeted or OTP is wrong';
             return response($data, 401);
         } elseif ($now->isAfter($verificationCode->expire_at)) {
             // return redirect()->route('otp.login')->with('error', 'Your OTP has expired');
@@ -154,28 +163,29 @@ class AuthController extends Controller
             'expire_at' => now(),
         ]);
         $user = User::where('phone', $request->phone)->first();
-
-        if($user){
-        $authToken = $user->createToken('auth-token')->plainTextToken;
-        return response()->json([
-            'status' => true,
-            'message' => 'User verefied successfully',
-            'access_token' => $authToken,
-        ]);
-         }
-         if(!$user){
+        if ($user) {
+            $authToken = $user->createToken('auth-token')->plainTextToken;
+            Auth::attempt([$request->phone, $request->password]);
+            return response()->json([
+                'status' => true,
+                'message' => 'User verefied and logged in successfully',
+                'access_token' => $authToken,
+            ]);
+        }
+        if (!$user) {
             $user = new User;
-        $user->name =  $request->name;
-        $user->phone = $request->phone;
-        $user->password = bcrypt( $request->password);
-        $user->save();
-        $authToken = $user->createToken('auth-token')->plainTextToken;
+            $user->name =  $request->name;
+            $user->phone = $request->phone;
+            $user->password = bcrypt($request->password);
+            $user->save();
+            $authToken = $user->createToken('auth-token')->plainTextToken;
+            Auth::attempt($request->only('phone', 'password'));
 
-        return response()->json([
-            'status' => true,
-            'message' => 'User created and verefied successfully',
-            'access_token' => $authToken,
-        ], 201);
+            return response()->json([
+                'status' => true,
+                'message' => 'User created and verefied and logged in successfully',
+                'access_token' => $authToken,
+            ], 201);
         }
 
         /// return redirect()->route('otp.login')->with('error', 'Your OTP is not correct');
